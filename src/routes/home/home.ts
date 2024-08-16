19131ed6275ab08a62dabed94199df0668f4fbcc
Paul Hovley <rphovley@gmail.com>
2024-08-16 14:48:14 -0600

    Make codeclimbers localhost your home page

---
+++ src/routes/home/home.ts
@@ -0,0 +1,11 @@
+//check if server is running on localhost
+fetch("http://localhost:14400")
+.then((response) => {
+  if (response.status === 200) {
+    window.location.href = "http://localhost:14400";
+  }
+})
+.catch((error) => {
+  console.log("Server is not running on localhost");
+  window.location.href = "https://codeclimbers.io";
+});
